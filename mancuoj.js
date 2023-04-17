695fd8b48c5777c79dc63b7f8da930751698dade
mancuoj <1337381768@qq.com>
2023-04-17 16:46:58 +0800

    step 1

--- mancuoj.js
+++ mancuoj.js
@@ -1,17 +1,34 @@
-const element = {
-  type: 'h1',
-  props: {
-    title: 'foo',
-    children: 'Hello',
+function createElement(type, props, ...children) {
+  return {
+    type,
+    props: {
+      ...props,
+      children: children.map(child => typeof child === "object" ? child : createTextElement(child)),
+    }
   }
 }
-const container = document.getElementById('root')
 
-const node = document.createElement(element.type)
-node['title'] = element.props.title
+function createTextElement(text) {
+  return {
+    type: "TEXT_ELEMENT",
+    props: {
+      nodeValue: text,
+      children: [],
+    },
+  }
+}
 
-const text = document.createTextNode('')
-text['nodeValue'] = element.props.children
+const Mancuoj = {
+  createElement,
+}
 
-node.appendChild(text)
-container.appendChild(node)
+/** @jsx Mancuoj.createElement */
+const element = (
+  <div id="foo">
+    <a>bar</a>
+    <b />
+  </div>
+)
+
+const container = document.getElementById("root")
+ReactDOM.render(element, container)
