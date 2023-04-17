af6f9df5f8cd2c1952e9e12967fe5fca124573eb
mancuoj <1337381768@qq.com>
2023-04-17 16:56:16 +0800

    step 2

--- mancuoj.js
+++ mancuoj.js
@@ -18,17 +18,33 @@ function createTextElement(text) {
   }
 }
 
+function render(element, container) {
+  const dom = element.type === 'TEXT_ELEMENT' ? document.createTextNode('') : document.createElement(element.type)
+
+  Object.keys(element.props)
+    .filter(key => key !== "children")
+    .forEach(key => {
+      dom[key] = element.props[key]
+    })
+
+  element.props.children.forEach(child => {
+    render(child, dom)
+  })
+  container.appendChild(dom)
+}
+
 const Mancuoj = {
   createElement,
+  render,
 }
 
 /** @jsx Mancuoj.createElement */
 const element = (
   <div id="foo">
-    <a>bar</a>
-    <b />
+    <h1 style="color: red;">Hello World</h1>
+    <p style="color: green;">Mancuoj</p>
   </div>
 )
 
 const container = document.getElementById("root")
-ReactDOM.render(element, container)
+Mancuoj.render(element, container)
